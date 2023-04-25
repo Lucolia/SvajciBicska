5dda7a70de27d271ec31956c3fd8021d85209889
Lucolia <128601324+Lucolia@users.noreply.github.com>
2023-04-26 01:27:32 +0200

    Add files via upload

---
+++ Leghosszabbszo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Leghosszabbszo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("szavak.txt");
+            List<string> szavak = new List<string>();
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                szavak.Add(sorok[i]);
+            }
+            string leghosszabb = "";
+            foreach (string szo in szavak)
+            {
+                if (szo.Length > leghosszabb.Length)
+                {
+                    leghosszabb = szo;
+                }
+            }
+            Console.WriteLine($"A leghosszabb szó: {leghosszabb}");
+        }
+    }
+}
