c53c8f7afc537e8749834c92abe44c328732132e
Lucolia <128601324+Lucolia@users.noreply.github.com>
2023-04-26 00:08:30 +0200

    Add files via upload

---
+++ Listatolto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listatolto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>(); 
+
+            Console.WriteLine("Kérlek adj meg egy szöveget!");
+            while (true)
+            {
+                string szoveg = Console.ReadLine(); 
+
+                if (string.IsNullOrWhiteSpace(szoveg)) 
+                {
+                    break;
+                }
+                else
+                {
+                    lista.Add(szoveg); 
+                }
+            }
+        }
+    }
+}
